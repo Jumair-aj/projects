899a9b8f983eb1b8386d5be2e5a21112f7c164a6
Jumair-aj <83821197+Jumair-aj@users.noreply.github.com>
2022-10-20 15:56:26 +0530

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,23 @@
+<?php 
+$errors = '';
+$myemail = 'developerswelkinwits@gmail.com';//<-----Put Your email address here.
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$phone = $_POST['phone']; 
+$message = $_POST['message']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission from  PT LAM: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Phone: $phone \n Email: $email \n Message: \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: success.html');
+} 
+?>
